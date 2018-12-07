940ac5e5a6e4ce711a54665c902e0825410b422a
Madeline Brown <madsbrwn@gmail.com>
2018-12-06 22:04:23 -0700

    no message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <button 
           v-on:click="getFonts" type="button" 
           class="btn btn-warning btn-lg btn-block"
-          style="color: #fff"
+          style="color: #fff; font-weight: bold"
         >
           Refresh
         </button>
